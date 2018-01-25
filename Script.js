ad4e73135c45eeb55cf392e0a8b4425e718e39e8
Renate Weigl <renate.weigl@gmail.com>
2018-01-25 11:26:57 +0100

    back to top

--- Script.js
+++ Script.js
@@ -34,5 +34,3 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
